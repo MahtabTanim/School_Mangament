655fe8bda46875096034d6ba9d6e437f74dbf2d3
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2024-11-27 12:26:12 +0600

    updated readme

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,16 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": 'hello',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
+from django.urls import path
+from student_app import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('add-techer/', views.show_teacher_info, name='show_teacher_info')
 ]
